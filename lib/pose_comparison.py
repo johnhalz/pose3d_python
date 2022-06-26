a4473c75b6957e6660657f49b72059868ce7dd1b
John Halazonetis <john.halazonetis@icloud.com>
2022-06-26 17:52:13 +0200

    New class to compare poses (not finished)

---
+++ lib/pose_comparison.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from pose import Pose
+from pose2d import Pose2D
+
+
+class PoseComparison:
+    @staticmethod
+    def different_pose(pose_1, pose_2) -> bool:
+        pass
+
+
+    @staticmethod
+    def calc_difference(pose_1, pose_2):
+        pass
