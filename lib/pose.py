51b5f84e4470d277f7f941b7bdbe7ab2088be778
John Halazonetis <john.halazonetis@beyondgravity.com>
2022-07-06 15:00:58 +0200

    Added name member variable

--- lib/pose.py
+++ lib/pose.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 class Pose:
-    def __init__(self) -> None:
-        self.rotation = R.identity()
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.rotation = Rotation.identity()
         self.position = np.array([0.0, 0.0, 0.0])
--- lib/pose2d.py
+++ lib/pose2d.py
@@ -2,6 +2,7 @@ import numpy as np
 from rotation2d import Rotation2D
 
 class Pose2D:
-    def __init__(self) -> None:
+    def __init__(self, name) -> None:
+        self.name = name
         self.rotation = Rotation2D()
         self.position = np.array([0.0, 0.0])
