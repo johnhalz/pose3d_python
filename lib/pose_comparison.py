fb0c08596c57d1eebd36c475d1921de6f80608c8
John Halazonetis <john.halazonetis@beyondgravity.com>
2022-07-07 15:08:36 +0200

    Fixed calc_difference method

--- lib/pose_comparison.py
+++ lib/pose_comparison.py
@@ -20,11 +20,12 @@ class PoseComparison:
     def calc_difference(pose_1, pose_2, degrees: bool = False):
         position_difference = np.subtract(pose_1.position, pose_2.position)
 
-        if type(pose_1) is Pose2D and type(pose_2) is Pose2D:
+        if position_difference.shape == (2,):
             rotation_difference = abs(pose_1.rotation.as_euler(degrees) - pose_2.rotation.as_euler(degrees))
-        elif type(pose_1) is Pose and type(pose_2) is Pose:
-            mat = pose_1.rotation.inv.as_matrix() @ pose_2.rotation.as_matrix()
+
+        elif position_difference.shape == (3,):
+            mat = np.matmul(pose_1.rotation.inv().as_matrix(), pose_2.rotation.as_matrix())
             compound_rot = Rotation.from_matrix(mat)
-            rotation_difference = np.linalg.norm(compound_rot.as_rotvec(degrees=degrees))
+            rotation_difference = np.linalg.norm(compound_rot.as_rotvec())
 
         return position_difference, rotation_difference
