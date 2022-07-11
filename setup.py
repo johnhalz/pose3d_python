7db9fe5e896fc57c4c4f28164553eb9682733351
John Halazonetis <john.halazonetis@icloud.com>
2022-07-11 08:33:01 +0200

    Automatically include packages from requirements file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 VERSION = '1.1.0'
 DESCRIPTION = 'Transforming and handling poses.'
 
-# read the contents of your README file
+# Read the contents of README file
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
@@ -18,7 +18,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=['numpy', 'scipy'],
+    install_requires=open("requirements.txt", "r").read().split("\n"),
     keywords=['python', 'pose', 'transform'],
     classifiers=[
         "Intended Audience :: Developers",
