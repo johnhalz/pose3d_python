db3431594f40f4b7d9d801f4eb48cff5a325691c
John Halazonetis <john.halazonetis@icloud.com>
2022-07-11 08:35:45 +0200

    Remove unnecessary comments

--- pose3d/__init__.py
+++ pose3d/__init__.py
@@ -1,10 +1,3 @@
-# from pose3d.pose_comparison import PoseComparison
-# from pose3d.pose import Pose
-# from pose3d.pose2d import Pose2D
-# from pose3d.rotation2d import Rotation2D
-# from pose3d.transform_set import TransformSet
-# from pose3d.transform import Transform
-
 from .pose import Pose
 from .pose_comparison import PoseComparison
 from .pose2d import Pose2D
