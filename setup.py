757822e2ab7408b053effce1c7e793b358ba4a3d
John Halazonetis <john.halazonetis@icloud.com>
2022-09-26 07:45:39 +0200

    Added pandas and formatting

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
 VERSION = '1.1.4b1'
 DESCRIPTION = 'Transforming and handling 3D poses and frames.'
 
 # Read the contents of README file
-from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 # Add resource links
 project_urls = {
-  'Documentation': 'https://johnhal.gitlab.io/pose_python',
-  'Repository': 'https://github.com/johnhalz/pose3d'
+    'Documentation': 'https://johnhal.gitlab.io/pose_python',
+    'Repository': 'https://github.com/johnhalz/pose3d'
 }
 
 # Setting up
@@ -24,9 +24,9 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=['numpy', 'scipy'],
+    install_requires=['numpy', 'scipy', 'pandas'],
     keywords=['python', 'pose', 'transform'],
-    project_urls = project_urls,
+    project_urls=project_urls,
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
