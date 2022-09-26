b46f48594ef17e25d765c192f247d2c79cc2ad1f
John Halazonetis <john.halazonetis@beyondgravity.com>
2022-09-26 08:36:02 +0200

    v1.1.4 Release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
-VERSION = '1.1.4b1'
+VERSION = '1.1.4'
 DESCRIPTION = 'Transforming and handling 3D poses and frames.'
 
 # Read the contents of README file
