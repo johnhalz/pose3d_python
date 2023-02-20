c935f4d5e00c1db6e8d667379422b732f45bc8c6
John Halazonetis <john.halazonetis@icloud.com>
2023-02-20 08:54:50 +0100

    Updates to attr class and tester

--- demo/attr_et.py
+++ demo/attr_et.py
@@ -4,12 +4,12 @@ path.append(Path(__file__).parents[1].as_posix())
 
 from pose3d import ETAttr
 
-et_1 = ETAttr(name='TR1')
+et_1 = ETAttr.in_dim(dim=3)
 et_1.random()
 print(et_1)
 
 et_2 = ETAttr(name='TR2')
-et_2.random()
+et_2.vector = et_1.vector
 print(et_2)
 
-print(et_1 == et_2)
+assert (et_1 == et_2)
